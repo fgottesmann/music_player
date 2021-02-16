f582a1a105f670c35b7d262636ae31bc4178f2a3
fgottes <f.gottesmann@outlook.com>
2021-02-16 16:32:11 +0100

    Update: false naming convention in file names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Greeting from "../components/greeting";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>PewPewMusicplayer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}></footer>
+      <Greeting name="Fabian" />
     </div>
   );
 }
