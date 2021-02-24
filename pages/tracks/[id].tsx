abd633bc4fca353b2684612a7a08533c0aee68ed
fgottes <f.gottesmann@outlook.com>
2021-02-24 15:32:04 +0100

    Add new featue: Use local storage

--- pages/tracks/[id].tsx
+++ pages/tracks/[id].tsx
@@ -7,12 +7,20 @@ import styles from "../../styles/Trackpage.module.css";
 import SingleTrack from "../../components/singleTrack";
 import Player from "../../components/AudioPlayer1";
 import Head from "next/head";
+import useLocalStorage from "../../hooks/useLocalStorage";
+import Id from "../api/tracks/[id]";
 
 export default function Track() {
   const router = useRouter();
   const { id } = router.query;
 
   const [track, setTrack] = useState<APITrack>(null);
+
+  const [favoriteSongs, setFavoriteSongs] = useLocalStorage(
+    "favoriteSongs",
+    []
+  );
+  const favorite = favoriteSongs.includes(id);
 
   useEffect(() => {
     if (typeof id !== "string") {
@@ -22,6 +30,17 @@ export default function Track() {
       setTrack(newTrack);
     });
   }, [id]);
+
+  const handleFavoriteClick = () => {
+    if (favorite) {
+      const newFavoriteSongs = favoriteSongs.filter(
+        (favoriteSong) => favoriteSong !== Id
+      );
+      setFavoriteSongs(newFavoriteSongs);
+    } else {
+      setFavoriteSongs([...favoriteSongs, id]);
+    }
+  };
 
   if (!track) {
     return <div>Loading...</div>;
@@ -49,6 +68,9 @@ export default function Track() {
           {/* <Player fileUrl={url} /> */}
         </>
       </main>
+      <button className={styles.favbutton} onClick={handleFavoriteClick}>
+        {favorite ? "💘" : "🖤"}
+      </button>
       <footer>
         <Player fileUrl={track.url} />
       </footer>
